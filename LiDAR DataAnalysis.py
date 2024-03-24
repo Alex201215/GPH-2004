223abe57926fa37ee25ca9906e6b1ff4b535802e
Clément Poulin <117486857+pimentpoulet@users.noreply.github.com>
2024-03-24 17:54:20 -0400

    LiDAR DataAnalysis.py

--- LiDAR DataAnalysis.py
+++ LiDAR DataAnalysis.py
@@ -11,6 +11,7 @@ volts_2 = [float(x) for count,x in enumerate(data['2']) if count != 0]
 sos = scipy.signal.butter(1, 0.01, 'lowpass', output='sos')
 filtered_1 = scipy.signal.sosfilt(sos, volts_1)
 filtered_2 = scipy.signal.sosfilt(sos, volts_2)
+
 plt.plot(time[320:], volts_1[320:], label='Original signal')
 plt.plot(time[320:], volts_2[320:], label='Reflected signal')
 plt.plot(time[320:], filtered_1[320:], color='black', label='filtered signals')
